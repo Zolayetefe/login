2871fa33ba7fed9286bfc869b11975c718d16445
Zelalem Tefera <zedoyetefe@gmail.com>
2024-11-26 01:42:36 +0300

    complate interface of login except interacttion

--- lib/login_register_page.dart
+++ lib/login_register_page.dart
@@ -5,47 +5,50 @@ class LoginRegisterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-       padding: const EdgeInsets.only(top:50,bottom: 20,),
-       child: Column(
-         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 20),
-            child: Container(
-              height: MediaQuery.of(context).size.height/3,
-              width: MediaQuery.of(context).size.width,
-              color: Colors.white,
-              child: Center(
-                child:Image.asset("asset/pregnant.png",)
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+         padding: const EdgeInsets.only(top:50,bottom: 20,),
+         child: Column(
+           children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Container(
+                height: MediaQuery.of(context).size.height/3,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.white,
+                child: Center(
+                  child:Image.asset("asset/pregnant.png",)
+                ),
               ),
             ),
-          ),
-          const SizedBox(height:50),
-          Container(
-            child: const Column(
-              children: [
-                 Text(
-                  "Descover Your" ,textAlign:TextAlign.center,style: TextStyle(fontFamily:'Roboto',fontSize:36,fontWeight:FontWeight.bold ,color:Colors.blue,wordSpacing: 2)),
-                  SizedBox(height: 10),
+            const SizedBox(height:50),
+            Container(
+              child: const Column(
+                children: [
                    Text(
-                  "Dream Here" ,textAlign:TextAlign.center,style: TextStyle(fontFamily:'Roboto',fontSize:36,fontWeight:FontWeight.bold ,color:Colors.blue,wordSpacing: 2)),
-                  SizedBox(height: 20),
-                  Text("Explore all service based on your interest and fainancial capabilities",textAlign: TextAlign.center,style: TextStyle(fontFamily:'Roboto',fontSize: 12,color: Colors.grey,wordSpacing: 2),)
-              ],
-            )),
-            SizedBox(height:50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ElevatedButton(onPressed: (){}, child: const Text("Login")),
-                TextButton(onPressed: (){}, child: Text('Register'))
-              ],
-            ),
-         ],
-       ),
-       
+                    "Descover Your" ,textAlign:TextAlign.center,style: TextStyle(fontFamily:'Roboto',fontSize:36,fontWeight:FontWeight.bold ,color:Color(0xFF3A19F9),wordSpacing: 2)),
+                    SizedBox(height: 10),
+                     Text(
+                    "Dream Here" ,textAlign:TextAlign.center,style: TextStyle(fontFamily:'Roboto',fontSize:36,fontWeight:FontWeight.bold ,color:Color(0xFF3A19F9),wordSpacing: 2)),
+                    SizedBox(height: 20),
+                    Text("Explore all service based on your interest and fainancial capabilities",textAlign: TextAlign.center,style: TextStyle(fontFamily:'Roboto',fontSize: 12,color: Colors.grey,wordSpacing: 2),)
+                ],
+              )),
+              SizedBox(height:50),
+              Padding(padding: EdgeInsets.symmetric(horizontal: 60),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(onPressed: (){Navigator.pushNamed(context,"/login");},style:ElevatedButton.styleFrom(backgroundColor: Color(0xFF3A19F9)), child: const Text("Login",style:TextStyle(color: Colors.white)),),
+                  TextButton(onPressed: (){Navigator.pushNamed(context,'/register');}, child: Text('Register',style:TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 16)))
+                ],
+              ),),
+           ],
+         ),
+         
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'login_register_page.dart';
+import 'loginpage.dart';
+import 'registerpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +21,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginRegisterPage(),
+     initialRoute: "/",
+     routes: {
+      "/":(context)=> LoginRegisterPage(),
+      '/login':(context)=> LoginScreen(),
+      '/register':(context)=>RegisterScreen(),
+     },
     );
   }
 }
